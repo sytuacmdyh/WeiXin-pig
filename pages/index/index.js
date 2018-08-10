fa967c57863ca2414f1934cd81484e5bbbcba9a8
丁英豪 <dingyinghao@dm-maker.com>
2018-08-10 22:22:32 +0800

    授权+推送

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,11 +6,35 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     notes: [],
+    code: null,
+    openid: null,
   },
-  //事件处理函数
-  bindViewTap: function() {
+
+  onsubmit: function (e) {
+    wx.showToast({
+      title: this.data.openid,
+      duration: 10000,
+    })
+    console.log(this.data.openid)
+    wx.request({
+      url: 'https://www.yzccz.cn/api/wxSendTemplate',
+      data: {
+        formId: e.detail.formId,
+        touser: this.data.openid
+      },
+      header: {
+        'Accept': 'application/json',
+        'Authorization': app.globalData.apiToken,
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      dataType: 'json'
+    })
+  },
+
+  bindViewTap: function () {
     var tt = this;
-    
+
     wx.request({
       url: 'https://www.yzccz.cn/api/getNotes',
       header: {
@@ -18,25 +42,81 @@ Page({
         'Authorization': app.globalData.apiToken
       },
       success: function (res) {
-        console.log(res)
         tt.setData({
           notes: res.data
         })
       },
-      fail: function (res){
+      fail: function (res) {
         console.log(res)
       }
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
+
+  getExtraInfo: function () {
+    wx.login({
+      success: function (res) {
+        console.log(res)
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://www.yzccz.cn/api/wxLogin',
+            data: {
+              code: res.code
+            },
+            header: {
+              'Accept': 'application/json',
+              'Authorization': app.globalData.apiToken,
+              'content-type': 'application/json'
+            },
+            method: 'POST',
+            dataType: 'json',
+            success: function (res) {
+              console.log("xix", res)
+              wx.setStorage({
+                key: 'openid',
+                data: res.data.openid,
+              })
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
+  },
+
+  onShow: function () {
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
+    })
+    wx.getStorage({
+      key: 'userInfo',
+      success: function (res) {
+        that.setData({
+          userInfo: res.data
+        })
+        wx.getStorage({
+          key: 'openid',
+          success: function (res) {
+            that.setData({
+              openid: res.data
+            })
+          },
+          fail: function (e) {
+            that.getExtraInfo()
+          }
+        })
+      },
+      fail: function (e) {
+        wx.navigateTo({
+          url: '../home/home'
+        })
+      }
     })
   }
 })
